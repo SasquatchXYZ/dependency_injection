c49827b4010087096cc479a6e78c33df38dd2b0e
Dalton Ricker <dricker@wayfair.com>
2020-09-21 15:42:50 -0400

    changed to pull the type from an appsettings.json file

--- PureDI/Program.cs
+++ PureDI/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace PureDI
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace PureDI
 {
     class Program
     {
         static void Main(string[] args)
         {
-            IMessageWriter writer = new ConsoleMessageWriter();
+            // IMessageWriter writer = new ConsoleMessageWriter();
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string typeName = configuration["messageWriter"];
+            Type type = Type.GetType(typeName, throwOnError: true);
+
+            IMessageWriter writer = (IMessageWriter) Activator.CreateInstance(type);
+
             var salutation = new Salutation(writer);
             salutation.Exclaim("Hello DI!");
         }
