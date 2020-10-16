5d4dff79247f78a8f07c4416f3e1f5ff8d2270db
Dalton Ricker <dricker@wayfair.com>
2020-10-16 16:45:19 -0400

    added domain logic to the UWP project

--- UWPProductManagementClient/ProductManagement.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ProductManagement.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ UWPProductManagementClient/ProductManagement.Domain/Product.cs
@@ -0,0 +1,59 @@
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ProductManagement.Domain
+{
+    public class Product : INotifyPropertyChanged
+    {
+        private string _name;
+        private decimal _unitPrice;
+        private Guid _id;
+
+        public event PropertyChangedEventHandler PropertyChanged = (s, e) => { };
+
+        public Guid Id
+        {
+            get => _id;
+            set
+            {
+                if (_id != value)
+                {
+                    _id = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public decimal UnitPrice
+        {
+            get => _unitPrice;
+            set
+            {
+                if (_unitPrice != value)
+                {
+                    _unitPrice = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
